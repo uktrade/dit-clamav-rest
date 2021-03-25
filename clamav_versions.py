5c9af8aea0b860d6e3dcdf485c75e3b332210ab7
davecharles <22290851+davecharles@users.noreply.github.com>
2021-03-25 22:52:34 +0000

    feat: add service version info.
    - Added service version info.
    - Added new /check_version endpoint that reports service version
      as well as clamd local/remote version.
    - Added deprecated log warnings to old endpoints that should not
      be used.
    - Sanitised HTTP codes returned
    - Refactored version module and removed deprecated dns resolver
      call.
    - Updated README.
    - Added/refined tests.

--- clamav_versions.py
+++ clamav_versions.py
@@ -1,3 +1,7 @@
+"""Clam AV version helpers
+
+Inspired by https://forums.contribs.org/index.php?topic=49721.0
+"""
 import dns.resolver
 import logging
 import sys
@@ -5,55 +9,105 @@ import sys
 logger = logging.getLogger("CLAMAV-REST.VERSIONS_SERVICE")
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-'''
-Kudos to https://forums.contribs.org/index.php?topic=49721.0 
-'''
+
+class VersionError(Exception):
+    """Raised for version related errors."""
 
 
-def split_version(version_text, divider, offset):
+def parse_version(version_text, divider=":", offset=0):
+    """Parse a version string.
+
+    :param (str) version_text: Version text to parse.
+    :param (str) divider: Divider token.
+    :param (int) offset: Which slice.
+    :returns (str): Sliced element.
+    :raises: VersionError if text cannot be parsed.
+    """
     if not version_text:
-        raise Exception("version_text is required")
-
+        raise VersionError("version_text is required")
     split = version_text.split(divider)
-
     try:
         return split[offset]
     except IndexError:
-        raise Exception("version_text does not contain %d items" % offset + 1)
+        raise VersionError(f"version_text does not contain {offset + 1} items")
 
 
 def parse_local_version(version_text):
-    return split_version(version_text, '/', 1)
+    """Parse remote version.
+
+    Given something like 'ClamAV 0.100.3/25480/Fri Jun 14 08:12:45 2019'
+    extract '25480'.
+
+    :param (str) version_text: Remote version string.
+    :returns (str): Extracted version.
+    """
+    return parse_version(version_text, '/', 1)
+
+
+def parse_remote_version(version_text):
+    """Parse remote version.
+
+    Given something like '0.103.1:59:26120:1616678940:0:63:49191:333'
+    extract '26120'.
+
+    :param (str) version_text: Remote version string.
+    :returns (str): Extracted version.
+    """
+    return parse_version(version_text, ':', 2)
 
 
 def get_local_version_text(clamav):
-    version = clamav.version()
-    logger.info("local_version is %s" % version)
-    return version
+    """Interrogate local clamd for version.
 
-def get_local_version_number(clamav):
-    text = get_local_version_text(clamav)
-    if not text:
-        raise Exception(
-                "local_version_text is empty - is clamav running")
-    
-    return parse_local_version(text)
+    :param (ClamdNetworkSocket) clamav: clamd connection.
+    :returns (str): version string from clamd.
+    :raises: VersionError if version cannot be acquired.
+    """
+    try:
+        version = clamav.version()
+    except Exception as e:
+        msg = f"Failed to get local version info: {e}"
+        logger.error(msg)
+        raise VersionError(msg)
+    else:
+        logger.info(f"local version: {version}")
+        return version
 
-def parse_remote_version(version_text):
-    return split_version(version_text, ':', 2)
-
-def get_remote_version_number(uri):
-    text = get_remote_version_text(uri)
-    return parse_remote_version(text)
 
 def get_remote_version_text(uri):
-    answers = dns.resolver.query(uri, "TXT")
-    remote_version_text = ""
+    """Get remote version info through a DNS lookup.
 
-    for data in answers:
-        for txt_string in data.strings:
-            remote = txt_string.decode("utf-8")
-            remote_version_text = remote
+    :param (str) uri: Domain to perform lookup on.
+    :returns (str): latest ClamAV version.
+    """
+    try:
+        answers = dns.resolver.resolve(uri, "TXT")
+    except Exception as e:
+        msg = f"Failed to get remote version info: {e}"
+        logger.error(msg)
+        raise VersionError(msg)
+    else:
+        version = "unknown"
+        for data in answers:
+            for txt_string in data.strings:
+                version = txt_string.decode("utf-8")
+        logger.info(f"remote version: {version}")
+        return version
 
-    logger.info("remote_version is %s" % remote_version_text)
-    return remote_version_text
+
+def get_local_version_number(clamav):
+    """Get the local Clam AV version.
+
+    :param (ClamdNetworkSocket) clamav: clamd connection.
+    :returns (str): Local Clam AV version.
+    """
+    return parse_local_version(get_local_version_text(clamav))
+
+
+def get_remote_version_number(uri):
+    """Get remote Clam AV version.
+
+    :param (str) uri: Domain to use for version check.
+    :returns (str): Latest ClamAV version.
+    """
+    return parse_remote_version(get_remote_version_text(uri))
--- tests.py
+++ tests.py
@@ -11,6 +11,7 @@ import clamd
 
 import clamav_rest
 from clamav_versions import parse_local_version, parse_remote_version
+from version import __version__
 
 from flask_testing import LiveServerTestCase
 
@@ -24,75 +25,84 @@ EICAR_TEST_OUTPUTS = (
 
 
 def _get_auth_header(username, password):
-    creds = base64.b64encode(
-        bytes("{}:{}".format(username, password), "utf-8"))
-
+    creds = base64.b64encode(bytes("{}:{}".format(username, password), "utf-8"))
     return dict(Authorization=b'Basic ' + creds)
 
 
 def _get_file_data(file_name, data):
-    return dict(
-        file=(BytesIO(data), file_name))
+    return dict(file=(BytesIO(data), file_name))
 
 
 def _read_file_data(file_name):
     with open(file_name, "rb") as binary_file:
         data = binary_file.read()
-
-    return dict(
-        file=(BytesIO(data), file_name)
-    )
+    return dict(file=(BytesIO(data), file_name))
 
 
 class ClamAVVersionParsing(unittest.TestCase):
     def test_parse_local_version(self):
         expected_version = "12321"
         local_version_text = f"ClamAV 0.100.2/{expected_version}/Fri May 31 07:57:34 2019"
-
         local_version_number = parse_local_version(local_version_text)
-
         self.assertEqual(local_version_number, expected_version)
 
     def test_parse_remote_version(self):
         expected_version = "remote"
         remote_version_text = f"220.101.2:58:{expected_version}:1559294940:1:63:48725:32823"
-
         remote_version_number = parse_remote_version(remote_version_text)
-
         self.assertEqual(remote_version_number, expected_version)
 
 
 class ClamAVirusUpdate(unittest.TestCase):
-
     def setUp(self):
         clamav_rest.app.config['Testing'] = True
         self.app = clamav_rest.app.test_client()
 
-    @mock.patch("clamav_rest.get_remote_version_number")
-    @mock.patch("clamav_rest.get_local_version_number")
+    @mock.patch("clamav_rest.versions.get_remote_version_number")
+    @mock.patch("clamav_rest.versions.get_local_version_number")
     @mock.patch("clamav_rest.cd.ping")
     def test_local_remote_services_are_insync(self, ping, local, remote):
         remote.return_value = "1010101"
         local.return_value = "1010101"
-
         response = self.app.get("/check_warning")
-
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'1010101')
 
-    @mock.patch("clamav_rest.get_remote_version_number")
-    @mock.patch("clamav_rest.get_local_version_number")
+    @mock.patch("clamav_rest.versions.get_remote_version_number")
+    @mock.patch("clamav_rest.versions.get_local_version_number")
     def test_services_out_of_date(self, local, remote):
         remote.return_value = "25466"
         local.return_value = "25465"
-
         response = self.app.get("/check_warning")
-
         self.assertEqual(response.status_code, 500)
+
+    @mock.patch("clamav_rest.versions.get_remote_version_number")
+    @mock.patch("clamav_rest.versions.get_local_version_number")
+    @mock.patch("clamav_rest.cd.ping")
+    def test_local_remote_services_are_insync_v2(self, ping, local, remote):
+        remote.return_value = "1010101"
+        local.return_value = "1010101"
+        response = self.app.get("/check_version")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json["clamd-actual"], "1010101")
+        self.assertEqual(response.json["clamd-required"], "1010101")
+        self.assertEqual(response.json["outdated"], False)
+        self.assertEqual(response.json["service"], __version__)
+
+    @mock.patch("clamav_rest.versions.get_remote_version_number")
+    @mock.patch("clamav_rest.versions.get_local_version_number")
+    def test_services_out_of_date_v2(self, local, remote):
+        remote.return_value = "25466"
+        local.return_value = "25465"
+        response = self.app.get("/check_version")
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.json["clamd-actual"], "25465")
+        self.assertEqual(response.json["clamd-required"], "25466")
+        self.assertEqual(response.json["outdated"], True)
+        self.assertEqual(response.json["service"], __version__)
 
 
 class ClamAVRESTTestCase(unittest.TestCase):
-
     def setUp(self):
         clamav_rest.app.config['TESTING'] = True
         self.app = clamav_rest.app.test_client()
@@ -105,39 +115,23 @@ class ClamAVRESTTestCase(unittest.TestCase):
     @mock.patch("clamav_rest.cd.ping")
     def test_healthcheck_no_service(self, ping):
         ping.side_effect = clamd.ConnectionError()
-
         response = self.app.get("/check")
-
-        self.assertEqual(response.status_code, 502)
+        self.assertEqual(response.status_code, 503)
         self.assertEqual(response.data, b'Service Unavailable')
 
     @mock.patch("clamav_rest.cd.ping")
     def test_healthcheck_unexpected_error(self, ping):
         ping.side_effect = Exception("Oops")
-
         response = self.app.get("/check")
-
-        self.assertEqual(response.status_code, 500)
-        self.assertEqual(response.data, b'Service Unavailable')
-
-    @mock.patch("clamav_rest.cd.ping")
-    def test_healthcheck_unexpected_error_original(self, ping):
-        ping.side_effect = Exception("Oops")
-
-        response = self.app.get("/check")
-
-        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.status_code, 503)
         self.assertEqual(response.data, b'Service Unavailable')
 
     def test_scan_endpoint_requires_post(self):
         response = self.app.get("/scan")
-
         self.assertEqual(response.status_code, 405)
 
     def test_auth_ok(self):
-        response = self.app.post("/scan",
-                                 headers=_get_auth_header("app1", "letmein")
-                                 )
+        response = self.app.post("/scan", headers=_get_auth_header("app1", "letmein"))
 
         # expecting 400 as no file data
         self.assertEqual(response.status_code, 400)
@@ -147,7 +141,6 @@ class ClamAVRESTTestCase(unittest.TestCase):
                                  headers=_get_auth_header(
                                      "app1", "WRONGPASSWORD")
                                  )
-
         self.assertEqual(response.status_code, 401)
 
     def test_eicar(self):
@@ -156,7 +149,6 @@ class ClamAVRESTTestCase(unittest.TestCase):
                                  content_type='multipart/form-data',
                                  data=_get_file_data("unsafe.txt", EICAR)
                                  )
-
         self.assertEqual(response.data, b"NOTOK")
         self.assertEqual(response.status_code, 200)
 
@@ -205,20 +197,21 @@ class ClamAVRESTV2ScanTestCase(unittest.TestCase):
     def test_payload_right_size(self):
         # Fake content - 4.9Mb
         content = b"\0" * (clamav_rest.app.config['MAX_CONTENT_LENGTH'] - 10000)
-        response = self.app.post("/scan",
+        response = self.app.post("/v2/scan",
                                  headers=_get_auth_header("app1", "letmein"),
                                  content_type='multipart/form-data',
                                  data=_get_file_data(
                                      "4_9Mb.bin", content)
                                  )
-        self.assertEqual(response.data, b"OK")
+        self.assertEqual(response.json["malware"], False)
+        self.assertEqual(response.json["reason"], None)
         self.assertEqual(response.status_code, 200)
 
     def test_payload_too_large(self):
         logger = logging.getLogger('raven.base.Client')
         logger.disabled = True  # Sentry be quiet
         content = b"\0" * clamav_rest.app.config['MAX_CONTENT_LENGTH']
-        response = self.app.post("/scan",
+        response = self.app.post("/v2/scan",
                                  headers=_get_auth_header("app1", "letmein"),
                                  content_type='multipart/form-data',
                                  data=_get_file_data(
@@ -240,11 +233,13 @@ class ClamAVRESTV2ScanChunkedTestCase(LiveServerTestCase):
         self.headers["Transfer-encoding"] = "chunked"
         self.chunk_url = "http://localhost:5000/v2/scan-chunked"
 
-    def _eicar_gen(self):
+    @staticmethod
+    def _eicar_gen():
         yield b"X5O!P%@AP[4\PZX54(P^)7CC)7}$"
         yield b"EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
 
-    def _archive_file(self):
+    @staticmethod
+    def _archive_file():
         with open("client-examples/protected.zip", "rb") as binary_file:
             data = binary_file.read()
 
@@ -257,11 +252,12 @@ class ClamAVRESTV2ScanChunkedTestCase(LiveServerTestCase):
                 if not chunk:
                     break
 
-    def _text_file(self):
+    @staticmethod
+    def _text_file():
         yield b"NO VIRUS HERE"
         yield b"NO VIRUS HERE"
 
-    def test_encrypyed_archive(self):
+    def test_encrypted_archive(self):
         response = requests.post(
             self.chunk_url,
             headers=self.headers,
